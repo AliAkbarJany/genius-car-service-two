58b8a268b312e199784e4f2ef9d219adcaa11c33
Ali Akbar <aliakbarjany19@gmail.com>
2022-05-23 01:03:27 +0600

    second

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,27 +1,31 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import './Register.css'
 import auth from '../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import { updateProfile } from 'firebase/auth';
 
 const Register = () => {
+    const[agree,setAgree]=useState(false);
 
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
         error,
-      ] = useCreateUserWithEmailAndPassword(auth);
+      ] = useCreateUserWithEmailAndPassword(auth,{sendEmailVerification:true});
 
       const naviagete=useNavigate()
 
-      if(user){
-        naviagete('/home')
-      }
+    //   if(user){
+    //     naviagete('/home')
+    //   }
+
+      
 
         
-    const handleRegister=event=>{
+    const handleRegister= async(event)=>{
         event.preventDefault();
         // console.log(event.target) 
         // console.log(event.target.name)
@@ -33,9 +37,22 @@ const Register = () => {
         const email=event.target.email.value;
         const password=event.target.password.value;
         // console.log(name,email,password)
-        createUserWithEmailAndPassword(email, password);
-        
+
+        // const agree=event.target.terms.checked;
+         await createUserWithEmailAndPassword(email, password);
+        // if(agree){
+        //     createUserWithEmailAndPassword(email, password);
+        // }    
+        await updateProfile({ displayName:name });
+          console.log('Updated profile');
+          naviagete('/home')
+    
     }
+    let errorElement;
+        if(error){
+            errorElement= <p className='text-danger mt-2'>Error:: {error.message}</p>
+        }
+    
     return (
         <div className='register-form mt-5 '>
             <h2 className='text-primary text-center'>Please Register</h2>
@@ -46,8 +63,14 @@ const Register = () => {
                 <br />
                 <input  type="password" name="password" id="" placeholder='Password' required/>
                 <br />
-                <input type="submit" value="REGISTER" />
+                <input onClick={()=>setAgree(!agree)} type="checkbox" name="terms" id="terms" className='me- 2'/>
+                <label htmlFor="terms">Accept Terms & Conditaions</label>
+                
+                <input
+                    disabled={!agree}
+                    className='btn btn-primary w-50 mx-auto my-4' type="submit" value="REGISTER" />
             </form>
+            {errorElement}
             <p>Have An Account??? <Link to='/login' className='fw-bolder text-success'>PLEASE LOGIN</Link> </p>
 
             <SocialLogin></SocialLogin>
